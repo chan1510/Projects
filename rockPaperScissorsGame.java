4796a3602ec41e7b8e64cc54b7e880e0c36b7123
Chandrakant Chenva <chandrakantravat210@gmail.com>
2025-12-27 23:13:28 +0530

    Update rockPaperScissorsGame.java

--- rockPaperScissorsGame.java
+++ rockPaperScissorsGame.java
@@ -1,19 +1,25 @@
 import java.util.*;
-class Main{
-    public static void main(String[] args){
+
+class Main {
+    public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-        char p1,p2;
-        p1 = s.next().charAt(0);
-        p2 = s.next().charAt(0);
-        if(p1 == 'R' && p2 == 'P') System.out.println("P2 wins!");
-        else if(p1 == 'P' && p2 == 'R') System.out.println("P1 wins!");
-        else if(p1 == 'P' && p2 == 'S') System.out.println("P2 wins!");
-        else if(p1 == 'S' && p2 == 'P') System.out.println("P1 wins!");
-        else if(p1 == 'S' && p2 == 'R') System.out.println("P2 wins!");
-        else if(p1 == 'R' && p2 == 'S') System.out.println("P1 wins!");
-        else if (p1 == p2) System.out.println("Tie!");
-        else System.out.println("Invalid input");
+        int a = 0, b = 0;
+
+        for (int i = 0; i < 3; i++) {
+            char p1 = s.next().toUpperCase().charAt(0);
+            char p2 = s.next().toUpperCase().charAt(0);
+
+            if (p1 == 'R' && p2 == 'P') b++;
+            else if (p1 == 'P' && p2 == 'R') a++;
+            else if (p1 == 'P' && p2 == 'S') b++;
+            else if (p1 == 'S' && p2 == 'P') a++;
+            else if (p1 == 'S' && p2 == 'R') b++;
+            else if (p1 == 'R' && p2 == 'S') a++;
+        }
+
+        if (a > b) System.out.println("p1 wins!");
+        else if (a < b) System.out.println("p2 wins!");
+        else System.out.println("Tie!");
     }
-    
 }
